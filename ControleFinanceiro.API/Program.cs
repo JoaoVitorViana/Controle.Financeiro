ec979ecbc238bc481eb409fcbb2c43ca34b7c08c
João Viana <joaovitor@live.it>
2022-06-22 01:10:49 -0400

    first commit

---
+++ ControleFinanceiro.API/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Rewrite;
+using Swashbuckle;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(x =>
+{
+    x.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Controle Financeiro API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+//app.UseHttpsRedirection();
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("../swagger/v1/swagger.json", "Controle Financeiro API");
+});
+
+var option = new RewriteOptions();
+option.AddRedirect("^$", "/swagger");
+app.UseRewriter(option);
+
+app.Run();
